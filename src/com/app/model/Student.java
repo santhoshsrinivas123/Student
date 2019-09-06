ba5161d15f21db6e50dab965e67bb1d13a018354
lakshmikantha <lakshmikantha@DESKTOP-NIQ92QR>
2019-09-06 15:10:04 +0530

    add setters to rayudu

--- src/com/app/model/Student.java
+++ src/com/app/model/Student.java
@@ -3,5 +3,18 @@ package com.app.model;
 public class Student {
 	private String name;
 	private int id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 
 }
